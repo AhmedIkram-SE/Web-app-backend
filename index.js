9906450ec637d99b5ff1767e8477587be22b6bf4
Ahmed Ikram <ahmedikram2003@gmail.com>
2023-12-30 00:14:36 +0500

    first comit

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const {
   deleteStudent,
   updateStudent,
 } = require("./StudentsOperations");
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or specify a specific origin
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // or specify a specific origin
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+app.use(cors());
 
 app.use(express.json());
 
